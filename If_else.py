aa4ac34ff9aa4ec2c673a861cef9802fa6756b29
Shivani Rajput <114238450+shivaniirajput@users.noreply.github.com>
2023-04-21 23:13:08 +0530

    Add files via upload

---
+++ If_else.py
@@ -0,0 +1,27 @@
+a = 10
+b = 20
+
+#using if statement
+if b > a:
+  print("B is greater than A")
+  
+#using elis
+a = 10
+b = 10
+
+if a > b:
+  print("A is greater than B")
+
+elif a<b:
+  print("A is less than B")
+  
+else:
+  print("A equals B")
+
+#using pass
+a = 10
+b = 10
+
+if a > b:
+  print("a is greater than b")
+  pass
