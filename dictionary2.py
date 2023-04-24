67ca39927ff2e6e6c20e2510f843f450a3b0d829
Shivani Rajput <114238450+shivaniirajput@users.noreply.github.com>
2023-04-25 01:04:53 +0530

    Add files via upload

---
+++ dictionary2.py
@@ -0,0 +1,29 @@
+dic = {
+  "Name" : "Shivani",
+  "Course" : "Btech"
+ 
+}
+for x in dic:
+  print(x)                                             #print all keys
+  print(dic[x])                                        #print key and corresponding value in sequence
+
+dic = {
+  "Name" : "Shivani",
+  "Course" : "Btech"
+ 
+}
+for x in dic:
+  print(dic[x])                                        #print all values
+  
+dic = {
+  "Name" : "Shivani",
+  "Course" : "Btech"
+ 
+}
+dic1 = dic.copy()                                      #copies one dictionary element into another
+print(dic1)
+
+
+  
+
+  
