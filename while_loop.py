81b5aa2cd550dda4e3d498e1c7022495f2742c7b
Shivani <Shivanirajpoot998@gmail.com>
2023-04-25 01:37:08 +0530

    practice

---
+++ while_loop.py
@@ -0,0 +1,29 @@
+#WHILE LOOP
+i = 1
+while i<5:
+  print(i)                                                    #printing values between 1,5
+  i+=1
+  
+i = 1
+while i<5:
+  print(i)
+  if i==3:
+    break
+  
+  i+=1
+  
+i = 1
+while i<5:
+  i+=1
+  if i==3:
+    continue
+  print(i)
+  
+
+#FOR LOOP
+
+  
+  
+  
+
+
