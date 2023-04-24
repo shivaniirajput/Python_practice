292c6a475102caaeba2a2867056e098727fd302b
Shivani Rajput <114238450+shivaniirajput@users.noreply.github.com>
2023-04-25 01:05:12 +0530

    Add files via upload

---
+++ nestedDictionary.py
@@ -0,0 +1,50 @@
+Btech = {
+  "Branch" : {
+    "Course" : "Btech",
+    "Choice" : "one",
+    "Code" : "krlo"
+      
+      },
+  
+  "Branch1" : {
+    "OS" : "aata hai",
+    "MP" : "krlenge"
+  },
+  
+  "or kchh" : {
+    "bolo" : "haaan",
+    "achcha" : "han"
+  }
+  
+}
+
+print(Btech)                                                       #priniting all the dictionary  #nested dictionary
+
+#adding multiple dictionary and storing in new dictionary
+
+dic1 = {
+  "Name1" : "Shiv",
+  "Course1" : "Btech1"
+ 
+}
+dic2 = {
+  "Name2" : "Shivani",
+  "Course2" : "Btech2"
+ 
+}
+dic3 = {
+  "Name3" : "Shivanu",
+  "Course3" : "Btech3"
+ 
+}
+New_dic = {
+  "dic1" : "dic1",
+  "dic2" : "dic2",
+  "dic3" : "dic3"
+  
+}
+
+print(New_dic)                                                      #printing all three dictionary element
+
+
+
