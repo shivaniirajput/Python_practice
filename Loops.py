36d666e4b247a8f880552b8a18c262905dbb6089
Shivani <Shivanirajpoot998@gmail.com>
2023-04-26 12:24:32 +0530

    initial commit

---
+++ Loops.py
@@ -0,0 +1,25 @@
+#WHILE LOOP
+i = 1
+while i < 5 :
+  print(i)
+  i+=1               #if increment is not used then it will print a number infintely
+
+i =1  
+while i < 6:
+  print(i)
+  if (i == 3):
+    break            #by using break in a while loop we can break the loop to the end at any time either condition is true or not
+  i+=1
+  
+i =0  
+while i < 6:
+  i+=1
+  if i == 3:
+    continue           #by using continue in a while loop we can continue the loop by skipping a specific number
+  print(i)
+  
+  
+
+
+
+  
