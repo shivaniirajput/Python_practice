56769bc394df4bcac0be51946b0a71c05e4edabd
Shivani <Shivanirajpoot998@gmail.com>
2023-04-27 23:15:29 +0530

    code

---
+++ array1.py
@@ -0,0 +1,22 @@
+arr = ["A" , "B" , "C" , "D"]
+x = arr[0]
+print(x)                                                  #printing value through index
+
+arr = ["A" , "P" , "P" , "L" ,"E"]
+arr[0] = "S"
+print (arr)
+
+arr = ["A" , "P" , "P" , "L" , "E"]
+print(len(arr))
+
+arr = ["A" , "P" , "P" , "L" , "E"]
+for x in arr:
+  print(x)
+
+arr = ["A" , "P" , "P" , "L" , "E"]
+arr.append("B")
+print(arr)
+
+arr = ["A" , "P" , "P" , "L" , "E"]
+arr.pop(1)
+print(arr)
